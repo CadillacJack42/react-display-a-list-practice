847c84c065e8b8024a4310137d006f6bcee94b76
James Keicher <jamesdkeicher@gmail.com>
2022-01-27 12:34:59 -0800

    feat BandStats component complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 // import your arrays here
 import { bands } from './array-one';
 import Bands from './Bands/Bands';
+import { bandObjects } from './array-two';
+import BandStats from './BandStats/BandStats';
 
 function App() {
+  // console.log(bandObjects);
   return (
     <div className="App">
       <Bands bands={bands}/>
+      <BandStats bands={bandObjects}/>
     </div>
   );
 }
