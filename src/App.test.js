e090b34aee96ed05ba689092cc800c0b4971b67e
James Keicher <jamesdkeicher@gmail.com>
2022-01-27 16:27:47 -0800

    test: fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/led zeppelin/i);
+  const linkElement = screen.getByText(/band members/i);
   expect(linkElement).toBeInTheDocument();
 });
+
